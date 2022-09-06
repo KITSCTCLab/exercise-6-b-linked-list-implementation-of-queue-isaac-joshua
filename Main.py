87e120e0189f1fa86d209e4df0156a2e0bd9a032
isaac-joshua <110010536+isaac-joshua@users.noreply.github.com>
2022-09-06 12:13:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -36,12 +36,12 @@ class Queue:
 # Do not change the following code
 queue = Queue()
 operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
+
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
   if operations[i] == "enqueue":
     queue.enqueue(int(data[i]))
-
+  elif operations[i] == "dequeue":
+    queue.dequeue()
 queue.status()
